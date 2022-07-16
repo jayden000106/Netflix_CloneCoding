8722992d53904ba8d687998e5f4d7a092facfae7
jayden000106 <cars0106@icloud.com>
2022-07-16 22:16:28 +0900

    [TEST] #23 API 통신 테스트를 위한 모델 및 뷰모델, 뷰 추가

---
+++ NeflixCloneCoding/NeflixCloneCoding/Models/Respond.swift
@@ -0,0 +1,8 @@
+//
+//  Respond.swift
+//  NeflixCloneCoding
+//
+//  Created by 정지혁 on 2022/07/16.
+//
+
+import Foundation
---
+++ NeflixCloneCoding/NeflixCloneCoding/Views/Detail/APITestView.swift
@@ -0,0 +1,20 @@
+//
+//  APITestView.swift
+//  NeflixCloneCoding
+//
+//  Created by 정지혁 on 2022/07/16.
+//
+
+import SwiftUI
+
+struct APITestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct APITestView_Previews: PreviewProvider {
+    static var previews: some View {
+        APITestView()
+    }
+}
